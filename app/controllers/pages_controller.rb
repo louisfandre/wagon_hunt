fab3481b29602928cd35b513f38640218d5c9d12
Louis Andre <louisfandre@gmail.com>
2016-04-29 12:09:19 +0200

    Add product model

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,17 @@
 class PagesController < ApplicationController
+
   def home
+    @today = Date.today
+
   end
+
+  def team
+    @members = ["toto","tata","tutu"]
+
+  end
+
+  def join_us
+    render plain: "Send us a mail"
+  end
+
 end
