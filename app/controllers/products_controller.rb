9a75a7ad140dc2840bc2288dec59fe9306678adf
Louis Andre <louisfandre@gmail.com>
2016-05-10 15:40:46 +0200

    add Update and delete - Refacto controler, new and edit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,12 @@
 class ProductsController < ApplicationController
-
-
+  before_action :find_product, only: [:show, :edit, :update, :destroy]
 
   def index
     @products = Product.all
   end
 
   def show
-    @product = Product.find(params[:id])
+
   end
 
   def new
@@ -15,7 +14,6 @@ class ProductsController < ApplicationController
   end
 
   def create
-
     @product = Product.new(product_params)
     if @product.save
       redirect_to products_path
@@ -25,17 +23,29 @@ class ProductsController < ApplicationController
   end
 
   def edit
+
   end
 
   def update
+    if @product.update(product_params)
+      redirect_to products_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @product.destroy
+    redirect_to products_path
   end
 
   private
-
   def product_params
     params.require(:product).permit(:name, :url)
   end
+
+  def find_product
+    @product = Product.find(params[:id])
+  end
+
 end
