6429da721db04e5ee7258f8be2c8e23c4348843b
Louis Andre <louisfandre@gmail.com>
2016-05-10 11:58:38 +0200

    implement new and create for products - seed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,19 +11,31 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
   end
+
   def create
+
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
   end
+
   def edit
   end
+
   def update
   end
+
   def destroy
   end
 
+  private
 
-
-
-
-
+  def product_params
+    params.require(:product).permit(:name, :url)
+  end
 end
